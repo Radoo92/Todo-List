20cb775c602263954203d62464d2958ea5b3ec8f
Radovan Lipka <radovan.lipka@gmail.com>
2023-06-09 16:26:53 +0200

    typescript

---
+++ newTodo/todo-list/src/views/DeletedView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="deleted">
+    <h1>This is an deleted page</h1>
+    <ul>
+      <li
+        v-for="(item, index) in itemsToDisplay"
+        :key="index"
+        :class="{ deleted: item.deleted }"
+      >
+        {{ item.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { TodoItem, key } from "@/store";
+import { useStore } from "vuex";
+
+export default defineComponent({
+  name: "DeletedView",
+  computed: {
+    itemsToDisplay() {
+      return useStore(key).state.items.filter((item) => item.deleted);
+    },
+  },
+});
+</script>
---
+++ newTodo/todo-list/src/views/HomeView.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <h1>Todo App</h1>
+    <input v-model="newItemText" type="text" placeholder="Nová položka" />
+    <button class="btn btn-primary" @click="addItem">Pridať</button>
+    <ul>
+      <li
+        v-for="(item, index) in itemsToDisplay"
+        :key="index"
+        :class="{ deleted: item.deleted }"
+      >
+        {{ item.text }}
+        <button class="btn-delete" @click="deleteItem(item)">Vymazať</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { TodoItem, key } from "@/store";
+import { useStore } from "vuex";
+
+export default defineComponent({
+  name: "HomeView",
+  data() {
+    return {
+      newItemText: "",
+    };
+  },
+  setup() {
+    const store = useStore(key);
+    return {
+      store,
+    };
+  },
+  computed: {
+    itemsToDisplay() {
+      return useStore(key).state.items.filter(
+        (item: TodoItem) => !item.deleted
+      );
+    },
+  },
+  methods: {
+    addItem() {
+      if (this.newItemText) {
+        this.store.commit("addItem", {
+          text: this.newItemText,
+          deleted: false,
+        });
+        this.newItemText = "";
+      }
+    },
+    deleteItem(item: TodoItem) {
+      this.store.commit("deleteItem", item);
+    },
+  },
+});
+</script>
